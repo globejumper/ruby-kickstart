566dfc529a7b00eadb1dcb2e799660d2752c5f17
Lubos Michalic <lnmichalic@gmail.com>
2017-04-20 23:11:37 +0100

    Added 3_array skipped 2.2

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,7 +5,19 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+# 1. find even indices in the string and put them in an array
+# 2. Print out the array joined
+#
+#
 class String
   def every_other_char
+    arr = []
+    for i in 0..self.length-1 do
+      arr.push(self[i]) if i.even?
+
+    end
+    arr.join()
   end
 end
+
+puts "abcdefg".every_other_char
