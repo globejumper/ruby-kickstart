e7e761428ba3861c7caa56b78983f404636cf2c9
Lubos Michalic <lnmichalic@gmail.com>
2017-04-20 23:51:49 +0100

    Added 4_array

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,14 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+
+# 1. Define the methodet_squares that takes in an array
+# 2. Check each value X against all other values in the array looking for
+#    values where value = X * X store value in array
+# 3. return array
+def get_squares(arr)
+  results = []
+  arr.each {|x| results << x if arr.include?(x*x)}
+  results.sort
+end
