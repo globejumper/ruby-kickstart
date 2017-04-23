61a0b809c4b86abbd8835a645d066675672d4444
Lubos Michalic <lnmichalic@gmail.com>
2017-04-23 22:27:38 +0200

    added 3_hashes.rb

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,16 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+def word_count(string)
+  hash = {}
+  array = string.downcase.split
+  for x in array
+    number_of_instances = array.count(x)
+    hash[x] = number_of_instances
+  end
+  hash
+end
+str = "The quick brown fox jumps over the lazy doggo because the quick brown fox is so very quick and shifty you know"
+puts word_count(str)
