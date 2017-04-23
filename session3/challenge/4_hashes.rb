dd56d623057a2a228a815b6f7888dbead2c46370
Lubos Michalic <lnmichalic@gmail.com>
2017-04-23 22:41:55 +0200

    added 4_hashes

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,17 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+  hash = {}
+  array = string.split
+  for x in array do
+    position = array.index(x)
+    hash[x] = position
+  end
+  return hash
+end
+#
+# str = "The dog was no longer the lazy and so he chased the brown fox"
+#
+# puts first_pos(str)
